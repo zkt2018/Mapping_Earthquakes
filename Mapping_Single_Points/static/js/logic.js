d2b2090cfc6703df71eb2093d25207060255779d
zkt2018 <zky1384@yahoo.ca>
2021-05-30 11:25:13 -0400

    adding logic.js updates

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,11 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+//let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// Getting Los Angeles lat & long and zoomed in
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
 
 // OR Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -28,9 +32,36 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map); 
 
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map for Los Angeles
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// Add a light-yellow circle with black lines to the map for Los Angeles
+// L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: 'lightyellow',
+//     radius: 300
+//  }).addTo(map);
+
+// Add a light-yellow circle with black lines to the map for Los Angeles
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffffa1', // light yellow
+    radius: 300
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
