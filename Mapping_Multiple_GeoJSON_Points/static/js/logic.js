8cee1a6c95e4e38bfa271d93f7b2da8622793617
zkt2018 <zky1384@yahoo.ca>
2021-05-30 19:33:04 -0400

    Adding files

--- Mapping_Multiple_GeoJSON_Points/static/js/logic.js
+++ Mapping_Multiple_GeoJSON_Points/static/js/logic.js
@@ -34,73 +34,11 @@ let airportData = "https://raw.githubusercontent.com/zkt2018/Mapping_Earthquakes
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
   console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-    pointToLayer: function(feature, latlng) {
-      console.log(feature);
-      return L.marker(latlng).bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
-    }
-  }).addTo(map);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng).bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
+  }
+}).addTo(map);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Mapping multiple geojson points - first edition
-// Create the map object with center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-
-// // We create the tile layer that will be the background of our map. - lightmap
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
-// // Accessing the airport GeoJSON URL
-// let airportData = "https://raw.githubusercontent.com/zkt2018/Mapping_Earthquakes/main/majorAirports.json";
-
-// // Grabbing our GeoJSON data.
-// d3.json(airportData).then(function(data) {
-//   console.log(data);
-// // Creating a GeoJSON layer with the retrieved data.
-// L.geoJson(data, {
-//   pointToLayer: function(feature, latlng) {
-//     console.log(feature);
-//     return L.marker(latlng).bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
-//   }
-// }).addTo(map);
-// });
