3cc335db7a7eb8d28029419ff50da419b3fedfb1
zkt2018 <zky1384@yahoo.ca>
2021-05-30 11:41:55 -0400

    Adding updates

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,51 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
-
 // Getting Los Angeles lat & long and zoomed in
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-
-
-// OR Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-
- // We create the tile layer that will be the background of our map.
- // API URL with a reference to the accessToken
- // OpenStreetMap URL inside the curly braces of tileLayer() method
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//   maxZoom: 18,
-//   // mapbox/streets-v11 shows the streets on the map
-//   // different id can be used to change the map's style
-//   id: 'mapbox/streets-v11',
-//   tileSize: 512,
-//   zoomOffset: -1,
-//   accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map); 
-
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// Add a circle to the map for Los Angeles
-// L.circle([34.0522, -118.2437], {
-//     radius: 100
-//  }).addTo(map);
-
-// Add a light-yellow circle with black lines to the map for Los Angeles
-// L.circle([34.0522, -118.2437], {
-//     color: 'black',
-//     fillColor: 'lightyellow',
-//     radius: 300
-//  }).addTo(map);
 
 // Add a light-yellow circle with black lines to the map for Los Angeles
 L.circleMarker([34.0522, -118.2437], {
@@ -53,13 +10,6 @@ L.circleMarker([34.0522, -118.2437], {
     fillColor: '#ffffa1', // light yellow
     radius: 300
  }).addTo(map);
-
-// We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
