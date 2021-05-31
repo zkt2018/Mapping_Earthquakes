0b3d99abaeae64048f547d5687364624283e5804
zkt2018 <zky1384@yahoo.ca>
2021-05-30 23:54:54 -0400

    Adding file

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -101,22 +101,7 @@ style: styleInfo,
 }).addTo(earthquakes);
 // we add the earthquake layer to the map
 earthquakes.addTo(map);
-// Creating a GeoJSON layer with the retrieved data.
-// L.geoJson(data, {
-//   // We turn each feature into a circleMarker on the map.
-//   pointToLayer: function(feature, latlng) {
-//       console.log(data);
-//       return L.circleMarker(latlng);
-//     },
-//   // We set the style for each circleMarker using our styleInfo function.
-// style: styleInfo,
-//   // We create a popup for each circleMarker to display the magnitude and
-//   //  location of the earthquake after the marker has been created and styled.
-//   onEachFeature: function(feature, layer) {
-//   layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-// }
-// }).addTo(map);
-  
+
 });
 
 
